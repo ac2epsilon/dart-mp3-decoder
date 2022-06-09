00ec38f6663f56e13b005950bda3bdb117881a6d
Arsenii Chebotarov <ac2epsilon@gmail.com>
2022-06-09 22:02:19 +0300

    mp3-tech.org/programmer/frame_header.html

--- dart-1/bin/decode.dart
+++ dart-1/bin/decode.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 import 'dart:typed_data';
 import 'package:collection/collection.dart';
 import 'constants.dart' as constants;
-import 'frame/header.dart';
+import 'header.dart';
 
 const String file1 = constants.mp3file;
 const String file2 = constants.decfile;
@@ -11,7 +11,7 @@ void main(List<String> args) {
   Uint8List bytes = File(file1).readAsBytesSync();
   List<int> ffs = [];
   bytes.forEachIndexed((i, el) {
-    if (el == 0xff && (bytes[i + 1] & 0xf0) == 0xf0) ffs.add(i);
+    if (el == 0xff && (bytes[i + 1] & 0xe0) == 0xe0) ffs.add(i);
   });
   List<Header> h = ffs
       .map((el) => Header(bytes, el))
@@ -19,5 +19,5 @@ void main(List<String> args) {
       .where((el) => el.valid())
       .toList();
   print("ffs = ${ffs.length} filtered valid ${h.length}");
-  h[1].druk();
+  h[11].druk();
 }
--- dart-1/bin/header.dart
+++ dart-1/bin/header.dart
@@ -12,9 +12,25 @@ const bitrates = [
   [0, 32, 40, 48, 56, 64, 80, 96, 112, 128, 160, 192, 224, 256, 320, -1],
 ];
 
+const ids = ['MPEG-II 2.5', 'Reserved', 'MPEG-II', 'MPEG-I'];
+
+const layers = [
+  'Reserved',
+  'Layer III',
+  'Layer II',
+  'Layer I',
+];
+
+const samplings = [
+  [11025, 12000, 8000],
+  [-1, -1, -1], // id == 1, no such thing
+  [22050, 24000, 16000],
+  [44100, 48000, 32000]
+];
+
 class Element {
   String field;
-  int value;
+  num value;
   String legend;
   Element(this.field, this.value, this.legend);
 }
@@ -23,7 +39,7 @@ class Header {
   final Uint8List bytes; // byte array of whole file
   final int frame; // offset in array
 // byte +1
-  int get id => bitz(bytes[frame + 1], 0x08 /*0000 1000*/, 3);
+  int get id => bitz(bytes[frame + 1], 0x18 /*0001 1000*/, 3);
   int get layer => bitz(bytes[frame + 1], 0x06 /*0000 0110*/, 1);
   int get protec => bitz(bytes[frame + 1], 0x01 /*0000 0001*/, 0);
 // byte +2
@@ -32,6 +48,7 @@ class Header {
   int get bitrate => bitrateColumn >= 0 ? bitrates[bitrateColumn][btindex] : -1;
 
   int get sampling => bitz(bytes[frame + 2], 0x0c /*0000 1100*/, 2);
+
   int get padding => bitz(bytes[frame + 2], 0x02 /*0000 0010*/, 1);
   int get private => bitz(bytes[frame + 2], 0x01 /*0000 0001*/, 0);
 // byte +3
@@ -47,29 +64,32 @@ class Header {
     if (layer == 0) return false; // 00 is reserved value
     if (bitrateColumn < 0 || bitrateColumn > 4) return false;
     if (bitrate < 8 || bitrate > 448) return false; // including 0 and -1
+    if (sampling == 3 || detectSamplingFrequency == -1) return false;
     return true;
   }
 
   List<Element> hdrs = [];
 
   Header(this.bytes, this.frame) {
-    hdrs.add(Element('id', id, detectId(id)));
-    hdrs.add(Element('layer', layer, detectLayer(layer)));
-    hdrs.add(Element('protection_bit', protec, detectProtection(protec)));
+    hdrs.add(Element('id', id, detectId));
+    hdrs.add(Element('layer', layer, detectLayer));
+    hdrs.add(Element('protection_bit', protec, detectProtection));
 //- - - - - - - - - - - - - - -
     hdrs.add(Element('bitrate_index', btindex, 'index'));
     hdrs.add(Element('bitrate_column', bitrateColumn, '*calculated'));
-    hdrs.add(Element('bitrate_calculated', bitrate, 'kBit/s'));
+    hdrs.add(Element('bitrate_calculated', bitrate, '*kBit/s'));
 
-    hdrs.add(Element('sampling_frequency', sampling, ''));
-    hdrs.add(Element('padding_bit', padding, ''));
+    hdrs.add(Element('sampling', sampling, 'index'));
+    hdrs.add(Element('sampling_frequency', detectSamplingFrequency, '*Khz'));
+
+    hdrs.add(Element('padding_bit', padding, detectPadding()));
     hdrs.add(Element('private_bit', private, ''));
 //- - - - - - - - - - - - - - -
-    hdrs.add(Element('mode', mode, ''));
+    hdrs.add(Element('mode', mode, detectMode()));
     hdrs.add(Element('mode_extension', modeExt, ''));
-    hdrs.add(Element('copyright', copyright, ''));
-    hdrs.add(Element('original/home', origHome, ''));
-    hdrs.add(Element('emphasis', emphasis, ''));
+    hdrs.add(Element('copyright', copyright, detectCopyright()));
+    hdrs.add(Element('original/home', origHome, detectOriginal()));
+    hdrs.add(Element('emphasis', emphasis, detectEmphasis()));
   }
   void druk() {
     print(""" 
@@ -81,36 +101,29 @@ class Header {
     });
   }
 
-  String detectId(int v) {
+  String get detectId {
     var result = 'Unknown';
-    if (v == 0) result = 'MPEG-II';
-    if (v == 1) result = 'MPEG-I';
+    if (id >= 0 && id <= 3) result = ids[id];
     return result;
   }
 
-  String detectLayer(int v) {
+  String get detectLayer {
     var result = 'Unknown';
-    const layers = [
-      'Reserved',
-      'Layer III',
-      'Layer II',
-      'Layer I',
-    ];
-    if (v >= 0 && v <= 3) result = layers[v];
+    if (layer >= 0 && layer <= 3) result = layers[layer];
     return result;
   }
 
-  String detectProtection(int v) {
+  String get detectProtection {
     var result = 'Unknown';
-    if (v == 0) result = 'Protection On';
-    if (v == 1) result = 'Protection Off';
+    if (protec == 0) result = 'Protection On';
+    if (protec == 1) result = 'Protection Off';
     return result;
   }
 
   int detectBitrateColumn() {
     var result = -1;
-    if (id == 1) {
-      // MPEG II, Layer III, II, I
+    if (id == 3) {
+      // MPEG I, Layer III, II, I
       if (layer == 1) {
         result = 4;
       } else if (layer == 2) {
@@ -118,12 +131,54 @@ class Header {
       } else if (layer == 3) {
         result = 2;
       }
-    } else if (id == 0) {
+    } else if (id == 2 || id == 0 /* 2.5 */) {
       // MPEG II, Layer 1 and II+III
       if (layer == 3) {
         result = 0;
       } else if (layer == 2 || layer == 1) {
         result = 1;
+      }
+    }
+    return result;
+  }
+
+  int get detectSamplingFrequency {
+    if (id == 1 || sampling == 3) return -1;
+    return samplings[id][sampling];
+  }
+
+  String detectPadding() {
+    return padding == 1 ? 'Padding: Round' : 'Padding: Truncate';
+  }
+
+  String detectMode() {
+    const modes = ['stereo', 'joint_stereo', 'dual_channel', 'single_channel'];
+    return modes[mode] + (validateMode() ? ' VALID' : ' INVALID');
+  }
+
+  String detectCopyright() {
+    return copyright == 1 ? 'Copyright: Yes' : 'Copyright: Not';
+  }
+
+  String detectOriginal() {
+    return origHome == 1 ? 'Original' : 'Home';
+  }
+
+  String detectEmphasis() {
+    const emph = ['no emphasis', '50/15 microsec', 'reserved', 'CCITT J.17'];
+    return emph[emphasis];
+  }
+
+  bool validateMode() {
+    bool result = true;
+    if (layer == 2) {
+      result = false;
+      if ([32, 48, 56, 80].contains(bitrate) && mode == 3) {
+        result = true;
+      } else if ([64, 96, 112, 128, 160, 192].contains(bitrate)) {
+        result = true;
+      } else if ([224, 256, 320, 384].contains(bitrate) && mode != 3) {
+        result = true;
       }
     }
     return result;
